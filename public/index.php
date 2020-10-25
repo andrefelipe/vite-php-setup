cff7f98d4083be7b6fdf54d2ee36d6c24f637815
andré felipe <andre@andrefelipe.com>
2020-10-24 21:23:06 -0300

    removed favicon from Vite side, it's a concern of the PHP app

--- public/index.php
+++ public/index.php
@@ -17,7 +17,6 @@ require_once __DIR__ . '/helpers.php';
 
 <head>
     <meta charset="UTF-8">
-    <link rel="icon" href="/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vite App</title>
 
