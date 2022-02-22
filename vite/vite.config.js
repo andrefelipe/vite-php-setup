e03a71db4c8dad16c5115fd16749fa8b324c9ec0
andré felipe <andre@andrefelipe.com>
2022-02-22 16:36:13 -0300

    found the need to use path.resolve on input again, on new vite

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -13,7 +13,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import liveReload from 'vite-plugin-live-reload'
-
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -45,7 +45,7 @@ export default defineConfig({
 
     // our entry
     rollupOptions: {
-      input: '/main.js'
+      input: path.resolve(__dirname, 'src/main.js'),
     }
   },
 
